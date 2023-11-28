bb4cc0f41b92bf05a2d322ebcb5c0f0c1f9594a7
Sukanta Das <111263085+Sukantadas1@users.noreply.github.com>
2023-11-28 09:01:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 function stringChop(str, size) {
-  // your code here
+   // Check if the input string is null
+  if (str === null) {
+    return [];
+  }
+
+  // Initialize an array to store chunks
+  const chunks = [];
+
+  // Loop through the string with the specified chunk length
+  for (let i = 0; i < str.length; i += chunkLength) {
+    // Extract a chunk of the specified length
+    const chunk = str.substring(i, i + chunkLength);
+    // Add the chunk to the array
+    chunks.push(chunk);
+  }
+
+  return chunks;
 }
 
 // Do not change the code below
